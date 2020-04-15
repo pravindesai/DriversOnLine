43630361c5ac842d38cea9550fdcc61ce54d929b
pravin desai <pravindesai100@gmail.com>
2020-04-15 12:48:34 +0530

    {3}image in header

--- app/src/main/java/com/example/driversonline/ui/send/SendFragment.java
+++ app/src/main/java/com/example/driversonline/ui/send/SendFragment.java
@@ -20,16 +20,16 @@ public class SendFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        sendViewModel =
-                ViewModelProviders.of(this).get(SendViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_send, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
+
+        /*final TextView textView = root.findViewById(R.id.text_send);
         sendViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 }
