6fe9e45e96e4f16c121b28aa60c53d2c31fdf6db
pravin desai <pravindesai100@gmail.com>
2020-04-26 23:52:52 +0530

    {n} licence bug updated

--- app/src/main/java/com/example/driversonline/Cust_Dialog.java
+++ app/src/main/java/com/example/driversonline/Cust_Dialog.java
@@ -7,7 +7,6 @@ import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
--- app/src/main/java/com/example/driversonline/MainActivity.java
+++ app/src/main/java/com/example/driversonline/MainActivity.java
@@ -1,25 +1,16 @@
 package com.example.driversonline;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
 
 public class MainActivity extends AppCompatActivity {
     Button OwnerBtn,DriverBtn;
