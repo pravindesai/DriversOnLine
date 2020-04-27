47a94790373ec0ab3c6f93f99834ad0d7fae200a
pravin desai <pravindesai100@gmail.com>
2020-04-27 22:33:30 +0530

    {n} licence bug updated

--- app/src/androidTest/java/com/example/driversonline/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/driversonline/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.driversonline;
+package com.driver.driversonline;
 
 import android.content.Context;
 
@@ -16,12 +16,12 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class driverInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.driversonline", appContext.getPackageName());
+        assertEquals("com.driver.driversonline", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/driversonline/ExampleUnitTest.java
+++ app/src/test/java/com/example/driversonline/ExampleUnitTest.java
@@ -1,15 +1,15 @@
-package com.example.driversonline;
+package com.driver.driversonline;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * driver local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class driverUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
